c9d86e90de7687b0dab144c9e00543608b4755e4
vasc <nico@vasc.dev>
2025-01-28 23:45:14 +0100

    add plausible analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,7 @@
 interface ImportMetaEnv {
   readonly PUBLIC_DIRECTUS_URL: string;
+  readonly PUBLIC_PLAUSIBLE_URL: string;
+  readonly PUBLIC_PLAUSIBLE_DOMAIN: string;
 }
 
 interface ImportMeta {
